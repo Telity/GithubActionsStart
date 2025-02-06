321813839f838cdb392754ef673b34e26c04232b
Telity <Nigu0001@Stud.kea.dk>
2025-02-06 10:14:08 +0100

    initial

---
+++ src/test/java/yaml/githubactionsstart/GithubActionsStartApplicationTests.java
@@ -0,0 +1,13 @@
+package yaml.githubactionsstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
